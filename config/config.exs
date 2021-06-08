54328f448208ec0808f0de7aa72b2fa069c65cb3
Jimjule <roadknightj@gmail.com>
2021-06-08 11:36:57 +0100

    Sets up Ecto test env

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,5 @@
 import Config
 
-config :tic_tac_toe_elixir, TicTacToeElixir.Repo,
-  database: "tic_tac_toe_elixir_repo",
-#  username: "user",
-#  password: "pass",
-  hostname: "localhost"
-
-config :tic_tac_toe_elixir, :"TicTacToeElixir.repo",
-  database: "tic_tac_toe_elixir_repo",
-  hostname: "localhost"
-
 config :tic_tac_toe_elixir, ecto_repos: [TicTacToeElixir.Repo]
+
+import_config "#{Mix.env()}.exs"
