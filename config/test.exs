06cc5da950c6790ac82e8265f95c4559a67cf83a
Jimjule <roadknightj@gmail.com>
2021-06-08 15:34:02 +0100

    Sets postgres username and password for travis

--- config/test.exs
+++ config/test.exs
@@ -2,5 +2,7 @@ use Mix.Config
 
 config :tic_tac_toe_elixir, TicTacToeElixir.Repo,
        database: "tic_tac_toe_elixir_test",
+       username: "postgres",
+       password: "",
        hostname: "localhost",
        pool: Ecto.Adapters.SQL.Sandbox
