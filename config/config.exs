973aac4086676eeb3d1c656542f4d38f62f5a3bd
Jimjule <roadknightj@gmail.com>
2021-06-08 11:44:21 +0100

    Re-adds config in config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,13 @@
 import Config
 
+config :tic_tac_toe_elixir, TicTacToeElixir.Repo,
+  database: "tic_tac_toe_elixir_repo",
+  hostname: "localhost"
+
+config :tic_tac_toe_elixir, :"TicTacToeElixir.repo",
+  database: "tic_tac_toe_elixir_repo",
+  hostname: "localhost"
+
 config :tic_tac_toe_elixir, ecto_repos: [TicTacToeElixir.Repo]
 
 import_config "#{Mix.env()}.exs"
